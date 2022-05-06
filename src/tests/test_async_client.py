26b959227ccd1fa07865cb7d142b07c95e89bdc2
Marco Müllner <muellnermarco@gmail.com>
2022-05-06 21:35:15 +0200

    Initial commit

---
+++ src/tests/test_async_client.py
@@ -0,0 +1,63 @@
+import os
+
+import pytest
+
+from PyDiscordWrapper import AsyncClient, User, Guild, Channel, Role, Member
+
+@pytest.mark.asyncio
+async def test_auth():
+    client_id = 15616584861681
+    scope = ['identify', 'guilds']
+    redirect_uri = 'http://localhost:3000'
+    url = AsyncClient.auth(client_id, scope, redirect_uri)
+    assert url == 'https://discordapp.com/api/oauth2/authorize?client_id=15616584861681&scope=identify%20guilds&redirect_uri=http://localhost:3000'
+
+@pytest.mark.asyncio
+async def test_current_user():
+    auth_token = os.getenv('AUTH_TOKEN')
+    client = AsyncClient(auth_token)
+    user = await client.getCurrentUser(True)
+    assert isinstance(user, User)
+
+@pytest.mark.asyncio
+async def test_current_user_guilds():
+    auth_token = os.getenv('AUTH_TOKEN')
+    client = AsyncClient(auth_token)
+    guilds = await client.getCurrentUserGuilds(True)
+    assert isinstance(guilds, list)
+    assert isinstance(guilds[0], Guild)
+
+@pytest.mark.asyncio
+async def test_get_guild():
+    auth_token = os.getenv('AUTH_TOKEN')
+    client = AsyncClient(auth_token)
+    guilds = await client.getCurrentUserGuilds(True)
+    guild = await client.getGuild(guilds[0].id, True)
+    assert isinstance(guild, Guild)
+
+@pytest.mark.asyncio
+async def test_get_guild_channels():
+    auth_token = os.getenv('AUTH_TOKEN')
+    client = AsyncClient(auth_token)
+    guilds = await client.getCurrentUserGuilds(True)
+    channels = await client.getGuildChannels(guilds[0].id, True)
+    assert isinstance(channels, list)
+    assert isinstance(channels[0], Channel)
+
+@pytest.mark.asyncio
+async def test_get_guild_roles():
+    auth_token = os.getenv('AUTH_TOKEN')
+    client = AsyncClient(auth_token)
+    guilds = await client.getCurrentUserGuilds(True)
+    roles = await client.getGuildRoles(guilds[0].id, True)
+    assert isinstance(roles, list)
+    assert isinstance(roles[0], Role)
+
+@pytest.mark.asyncio
+async def test_get_guild_members():
+    auth_token = os.getenv('AUTH_TOKEN')
+    client = AsyncClient(auth_token)
+    guilds = await client.getCurrentUserGuilds(True)
+    members = await client.getGuildMembers(guilds[0].id, True)
+    assert isinstance(members, list)
+    assert isinstance(members[0], Member)
---
+++ src/tests/test_client.py
@@ -0,0 +1,63 @@
+import os
+
+import pytest
+
+from PyDiscordWrapper import Client, User, Guild, Channel, Role, Member
+
+
+def test_auth():
+    client_id = 15616584861681
+    scope = ['identify', 'guilds']
+    redirect_uri = 'http://localhost:3000'
+    url = Client.auth(client_id, scope, redirect_uri)
+    assert url == 'https://discordapp.com/api/oauth2/authorize?client_id=15616584861681&scope=identify%20guilds&redirect_uri=http://localhost:3000'
+
+
+def test_current_user():
+    auth_token = os.getenv('AUTH_TOKEN')
+    client = Client(auth_token)
+    user = client.getCurrentUser(True)
+    assert isinstance(user, User)
+
+
+def test_current_user_guilds():
+    auth_token = os.getenv('AUTH_TOKEN')
+    client = Client(auth_token)
+    guilds = client.getCurrentUserGuilds(True)
+    assert isinstance(guilds, list)
+    assert isinstance(guilds[0], Guild)
+
+
+def test_get_guild():
+    auth_token = os.getenv('AUTH_TOKEN')
+    client = Client(auth_token)
+    guilds = client.getCurrentUserGuilds(True)
+    guild = client.getGuild(guilds[0].id, True)
+    assert isinstance(guild, Guild)
+
+
+def test_get_guild_channels():
+    auth_token = os.getenv('AUTH_TOKEN')
+    client = Client(auth_token)
+    guilds = client.getCurrentUserGuilds(True)
+    channels = client.getGuildChannels(guilds[0].id, True)
+    assert isinstance(channels, list)
+    assert isinstance(channels[0], Channel)
+
+
+def test_get_guild_roles():
+    auth_token = os.getenv('AUTH_TOKEN')
+    client = Client(auth_token)
+    guilds = client.getCurrentUserGuilds(True)
+    roles = client.getGuildRoles(guilds[0].id, True)
+    assert isinstance(roles, list)
+    assert isinstance(roles[0], Role)
+
+
+def test_get_guild_members():
+    auth_token = os.getenv('AUTH_TOKEN')
+    client = Client(auth_token)
+    guilds = client.getCurrentUserGuilds(True)
+    members = client.getGuildMembers(guilds[0].id, True)
+    assert isinstance(members, list)
+    assert isinstance(members[0], Member)
